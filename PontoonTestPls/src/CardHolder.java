9ff4e9d7eab93f8ca6cf121002d5a1fb1249c59f
AMackenzie68 <43272827+AMackenzie68@users.noreply.github.com>
2018-11-02 08:45:53 +0000

    Update CardHolder.java

--- PontoonTestPls/src/CardHolder.java
+++ PontoonTestPls/src/CardHolder.java
@@ -5,8 +5,10 @@ public abstract class CardHolder {
     int total = 0;
 
     public void drawCard() {
+    //Basic card drawing method for use in player and dealer classes
         newcard = ((int)(Math.random() * 10.0D) + 1);
         this.total += newcard;
+        //adds to the total of the hand of each object
     }
 
 }
